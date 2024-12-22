99b5463d08c6b7551729c2ec2e902acc93985ff6
KHALID <khalidboumaghdar07@gmail.com>
2024-12-22 14:14:07 +0100

    j'ai créé méthode gestionClasses et l'appel au méthode Ajouter qu'on a créé

--- breif2/src/Classe.java
+++ breif2/src/Classe.java
@@ -27,7 +27,38 @@ private static int id;
                 ", nom='" + nom + '\'' +
                 '}';
     }    
+ public static void gestionClasses() {
+        System.out.println("\n--- Gestion des Classes ---");
+        System.out.println("1. Ajouter une classe");
+        System.out.println("2. Modifier une classe");
+        System.out.println("3. Supprimer une classe");
+        System.out.println("4. Afficher les classes");
+        System.out.print("Choix: ");
+        int choix = scanner.nextInt();
+        scanner.nextLine();
 
+        switch (choix) {
+            case 1 : ajouterClasse();
+            break;
+            //case 2 : modifierClasse();
+            break;
+            //case 3 : supprimerClasse();
+            break;
+            //case 4 : afficherClasses();
+            break;
+            default :
+                System.out.println("Choix invalide !");
+        }
+    }
+  private static void ajouterClasse() {
+
+        scanner.nextLine();
+        System.out.print("Nom: ");
+        String nom = scanner.nextLine();
+        classes.add(new Classe(id, nom));
+        System.out.println("Classe ajoutée !");
+        id++;
+    }
 
 
 }
