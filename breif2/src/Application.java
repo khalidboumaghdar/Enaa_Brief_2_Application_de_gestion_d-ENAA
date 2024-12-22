c04a82125f245d4c944b0faf41a2e5aa3fc72da9
KHALID <khalidboumaghdar07@gmail.com>
2024-12-22 14:33:26 +0100

    j'ai appelé les fonction déclarées (gestion apprennant, gestion formateur et gestion des classe)

--- breif2/src/Application.java
+++ breif2/src/Application.java
@@ -6,6 +6,30 @@ public class Application {
 
 
     public static void main(String[] args) {
-        
+         Scanner scanner = new Scanner(System.in);
+        int choix;
+        do {
+            System.out.println("\n--- Menu Principal ---");
+            System.out.println("1. Gestion des Apprenants");
+            System.out.println("2. Gestion des Formateurs");
+            System.out.println("3. Gestion des Classes");
+            System.out.println("4. Quitter");
+            System.out.print("Choix: ");
+             choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1 : Apprenant.gestionApprenants();
+                break;
+                case 2 : Formateur.gestionFormateurs();
+                break;
+                case 3: Classe.gestionClasses();
+                break;
+                case 4:
+                    System.out.println("Merci d'avoir utilisé l'application. Au revoir !");
+                break;
+                default :
+                    System.out.println("Choix invalide, veuillez réessayer !");
+            }
+        }while (choix!=4);
     }
 }
